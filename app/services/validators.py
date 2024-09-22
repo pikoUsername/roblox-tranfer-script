db615d4490d7b68e04ad21434c7e42d171780345
pikoUsername <galymzhan.amantaj@gmail.com>
2024-09-22 18:37:44 +0500

    small fix

--- app/services/validators.py
+++ app/services/validators.py
@@ -2,13 +2,18 @@ import re
 
 
 url_validator_re = re.compile(r"https?:\/\/(www)?\.roblox\.com\/game-pass\/(\d*)\/")
+full_url_validator_re = re.compile(r"https?:\/\/(www)?\.roblox\.com\/game-pass\/(\d*)/(\w*)?")
 
 
 def validate_game_pass_url(url: str) -> bool:
     match = url_validator_re.search(url)
 
-    if not match:
-        return False
+    if match:
+        return True
+
+    match = full_url_validator_re.search(url)
+    if match:
+        return True
 
     groups = match.groups()
 
